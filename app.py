be835d7c10d8a7c8f7f5ea8b9c7836b5c9a145f0
anutriv <anoop.trivedi@gmail.com>
2025-05-19 15:26:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -195,4 +195,4 @@ def download_file(filename):
     return jsonify({"error": "File not found or empty"}), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
